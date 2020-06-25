e120fa7a11ddca387e759fd31e1814753c80ab11
Howard Chen <pohaoche@usc.edu>
2020-06-25 16:08:48 -0700

    <feat> Use reselector to get cartItems and cartItemsCount
    * Install reselector@4.0.0
    * Add selector selectCartItems in CartDropdown
    * Add selector selectCartItemsCount in CartIcon

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,22 @@
+import { createSelector } from "reselect";
+
+// method 1: input selector
+// return a piece of state
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+// method 2: output selector
