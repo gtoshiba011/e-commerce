de4299cacd10df7521db53cc2a58996201818aac
Howard Chen <pohaoche@usc.edu>
2020-06-25 00:19:58 -0700

    <feat> Add button in CollectionItem
    * Style CustomButton and CollectionItem

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./collection-item.styles.scss";
+import CustomButton from "../custom-button/custom-button.component";
 
 const CollectionItem = ({ imageUrl, name, price }) => {
   return (
@@ -12,6 +13,7 @@ const CollectionItem = ({ imageUrl, name, price }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
+      <CustomButton inverted>Add to cart</CustomButton>
     </div>
   );
 };
