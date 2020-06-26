fed096714186d19946d8b9a1fe6e6e75d45895f5
Howard Chen <pohaoche@usc.edu>
2020-06-26 16:56:37 -0700

    <fix> Use another selector for CollectionOverview
    * Add selectShopCollectionsForOverview which return array for CollectionOverview
    * Use destructure in mapping each collection

--- src/components/collection-overview/collection-overview.component.jsx
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -5,26 +5,20 @@ import { createStructuredSelector } from "reselect";
 import "./collection-overview.styles.scss";
 
 import CollectionPreview from "../collection-preview/collection-preview.component";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import { selectShopCollectionsForOverview } from "../../redux/shop/shop.selectors";
 
 const CollectionOverview = ({ collections }) => {
   return (
     <div className="collection-overview">
-      {collections.map((collection) => {
-        return (
-          <CollectionPreview
-            key={collection.id}
-            title={collection.title}
-            items={collection.items}
-          />
-        );
+      {collections.map(({ id, ...restProps }) => {
+        return <CollectionPreview key={id} {...restProps} />;
       })}
     </div>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections,
+  collections: selectShopCollectionsForOverview,
 });
 
 export default connect(mapStateToProps)(CollectionOverview);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,9 +2,16 @@ import { createSelector } from "reselect";
 
 const selectShop = (state) => state.shop;
 
+// return an object
 export const selectShopCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+// return an array
+export const selectShopCollectionsForOverview = createSelector(
+  [selectShopCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
 );
 
 export const selectCollection = (collectionUrlParam) =>
