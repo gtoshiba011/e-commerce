d14e7e6af06fd53e1ec48a89e022e9769a61b325
Howard Chen <pohaoche@usc.edu>
2020-06-26 13:39:36 -0700

    <feat> Apply local storage by using redux-persist package
    * Install redux-persist@6.0.0
    * Wrap App by PersistGate
    * Wrap rootReducer by persistReducer
    * Wrap store by persistStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { persistStore } from "redux-persist";
+
 import rootReducer from "./root-reducer";
 
-const store = createStore(rootReducer, applyMiddleware(logger));
-
-export default store;
+const middlewares = [logger];
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
