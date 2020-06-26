09e77a9d94fb6c9ea749fdd58a8b5df479dafa36
Howard Chen <pohaoche@usc.edu>
2020-06-26 14:52:01 -0700

    <feat> Save Shop data in Redux state
    * Create shop state in Redux
    * Transfer Shop from class-based to functional component
    * Move shop.data to redux/shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,31 +1,28 @@
-import React, { Component } from "react";
-import SHOP_DATA from "./shop.data";
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
 import CollectionPreview from "../../components/collection-preview/collection-preview.component";
+import { selectShopCollections } from "../../redux/shop/shop.selectors";
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const ShopPage = ({ collections }) => {
+  return (
+    <div className="shop-page">
+      {collections.map((collection) => {
+        return (
+          <CollectionPreview
+            key={collection.id}
+            title={collection.title}
+            items={collection.items}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map((collection) => {
-          return (
-            <CollectionPreview
-              key={collection.id}
-              title={collection.title}
-              items={collection.items}
-            />
-          );
-        })}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopCollections,
+});
 
-export default ShopPage;
+export default connect(mapStateToProps)(ShopPage);
